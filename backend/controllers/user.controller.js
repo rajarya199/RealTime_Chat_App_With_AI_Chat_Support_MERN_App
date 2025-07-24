0a1d0e4d7f67f508d4f606f6b7760fb32d2fe332
rajarya199 <aryalrajan078@gmail.com>
2025-07-24 23:38:43 +0545

    user login controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,6 +2,7 @@ import UserModel from "../models/user.model.js";
 import { createUser } from "../services/user.services.js";
 import {validationResult} from "express-validator"
 export const createUserController=async(req,res)=>{
+     // Check for validation errors from express-validator middleware
    const errors=validationResult(req)
    if(!errors.isEmpty()){
 return res.status(400).json({errors:errors.array()})
@@ -16,4 +17,41 @@ const user=await createUser(req.body);
         res.status(400).send(error.message);
 
     }
+}
+
+export const loginController=async(req,res)=>{
+    const errors=validationResult(req);
+      if(!errors.isEmpty()){
+return res.status(400).json({errors:errors.array()})
+   }
+     const { email, password } = req.body;
+
+   try{
+     const user=await UserModel.findOne({email}).select('+password');
+   if(!user){
+    res.status(401).json({
+        errors:'user not found'
+    })
+   }
+   const isMatch=await user.isValidPassword(password)
+   if(!isMatch){
+    return res.status(401).json({
+        errors:"invalid credientials"
+    })
+   }
+
+       // Generate JWT token
+   const token=await user.generateJWT()
+
+return res.status(200).json({
+  message: 'Login successful',
+  user,
+  token
+});
+  
+   }
+   catch(error){
+    console.log("error during login",error)
+   }
+  
 }
