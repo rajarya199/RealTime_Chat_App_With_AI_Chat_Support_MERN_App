ec281faf4a69cbddeeaf03976fe03488639f0b4d
rajarya199 <aryalrajan078@gmail.com>
2025-07-26 00:51:29 +0545

    routes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Route,BrowserRouter,Routes } from 'react-router-dom'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+import Home from '../pages/Home'
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"element={<>home</>}/>
-        <Route path="/login" element={<>login</>}/>
+        <Route path="/"element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
       </Routes>
     </BrowserRouter>
   )
