c1eecff4c70c9389301b5fc4062085f6e2c6bf55
rajarya199 <aryalrajan078@gmail.com>
2025-07-25 11:01:54 +0545

    get profile info

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express"
-import { createUserController,loginController } from "../controllers/user.controller.js"
+import { createUserController,loginController,ProfileController } from "../controllers/user.controller.js"
 import { body } from "express-validator";
+import  * as authmiddleware from"../middleware/auth.middleware.js"
 const router=Router();
 router.post("/register",
      body('email').isEmail().withMessage('Email must be a valid email address'),
@@ -14,4 +15,7 @@ body('username').isLength({min:3}).withMessage("username must be of at least 3 c
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
     loginController
     )
+
+
+    router.get('/profile',authmiddleware.authUser,ProfileController)
 export default router
