457474b8d12a4a86dcbf600593aa92cae5782ec7
rajarya199 <aryalrajan078@gmail.com>
2025-07-25 22:10:21 +0545

    set routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import AppRoutes from './routes/AppRoutes'
 const App = () => {
   return (
-    <div>
-      hello react
-    </div>
+    <>
+      <AppRoutes/>
+    </>
   )
 }
 
