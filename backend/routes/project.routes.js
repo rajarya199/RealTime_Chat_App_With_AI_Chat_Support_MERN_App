2c085739f8c7b6269f5fe9f6e639580c5e4ec2ff
rajarya199 <aryalrajan078@gmail.com>
2025-07-28 00:53:48 +0545

    add user route

--- backend/routes/project.routes.js
+++ backend/routes/project.routes.js
@@ -1,6 +1,6 @@
 import {Router} from "express"
 import { body } from "express-validator";
-import { buildProject, getAllProject } from "../controllers/project.controller.js";
+import { addUserToProject, buildProject, getAllProject } from "../controllers/project.controller.js";
 import  * as authmiddleware from"../middleware/auth.middleware.js"
 const router=Router()
 
@@ -10,4 +10,10 @@ router.post('/create',authmiddleware.authUser,
 )
 
 router.get('/all',authmiddleware.authUser,getAllProject)
+router.put('/add-user', authmiddleware.authUser,
+    body('projectId').isString().withMessage('Project ID is required'),
+    body('users').isArray({ min: 1 }).withMessage('Users must be an array of strings').bail()
+        .custom((users) => users.every(user => typeof user === 'string')).withMessage('Each user must be a string'),
+        addUserToProject
+)
 export default router;
