ab11d343e2942dc5f0454a6f6f6ea68a29a1efd1
rajarya199 <aryalrajan078@gmail.com>
2025-07-27 21:50:52 +0545

    route-layout

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,13 +3,17 @@ import { Route,BrowserRouter,Routes } from 'react-router-dom'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
 import Home from '../pages/Home'
+import Layout from '../layout/Layout'
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"element={<Home/>}/>
+        <Route path='/' element={<Layout/>}>
+   <Route index element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
+        </Route>
+     
       </Routes>
     </BrowserRouter>
   )
