7b012897e3b3c7361134853d1c32316a2c2886eb
rajarya199 <aryalrajan078@gmail.com>
2025-07-27 21:50:38 +0545

    home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,31 @@
 import React,{useContext} from 'react'
 import { UserContext } from '../context/user.context'
+import HeroSection from '../components/HeroSection.jsx';
+import FeaturesSection from '../components/FeatureSection';
+import AISection from '../components/AiSection';
+import WelcomeUser from '../components/WelcomeUser.jsx';
 const Home = () => {
   const { user } = useContext(UserContext);
 
-  if (!user) {
-    return <div>Please log in to see your profile information.</div>;
-  }
+  // if (!user) {
+  //   return <div>Please log in to see your profile information.</div>;
+  // }
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Welcome, {user.username}!</h1>
-      <p><strong>Email:</strong> {user.email}</p>
 
+    < >
+    <div>
+{user ?(<>
+ <h1 className="text-2xl font-bold mb-4">Welcome, {user?.username}!</h1>
+      <p><strong>Email:</strong> {user?.email}</p>
+</>):<div> Please log in to see your profile information.</div>}
     </div>
+     <WelcomeUser/>
+<HeroSection/>
+<FeaturesSection/>
+<AISection/>
+
+    </>
   );
 };
 
