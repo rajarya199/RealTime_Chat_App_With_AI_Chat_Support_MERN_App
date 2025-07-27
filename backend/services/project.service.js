890d0e64d675af74ab5b726999f8d8251f60cf4a
rajarya199 <aryalrajan078@gmail.com>
2025-07-28 00:53:36 +0545

    useradd

--- backend/services/project.service.js
+++ backend/services/project.service.js
@@ -1,5 +1,5 @@
 import Project from "../models/project.model.js";
-
+import mongoose from "mongoose";
 export const createProject = async ({ userId, name }) => {
   try {
     if (!name) {
@@ -36,5 +36,55 @@ return userAllProjects
   catch(error){
       console.error('Error fetching projects by userId:', error.message);
     throw error;
+  }
+}
+
+export const addUserstoProj=async({projectId,users,userId})=>{
+  try{
+    if(!projectId){
+      throw new Error("project id is required")
+    }
+     if (!mongoose.Types.ObjectId.isValid(projectId)) {
+        throw new Error("Invalid projectId")
+    }
+    if(!users){
+      throw new Error("users are required")
+    }
+      if (!Array.isArray(users) || users.some(userId => !mongoose.Types.ObjectId.isValid(userId))) {
+        throw new Error("Invalid userId(s) in users array")
+    }
+      if (!userId) {
+        throw new Error("userId is required")
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+        throw new Error("Invalid userId")
+    }
+       const project = await Project.findOne({
+        _id: projectId,
+        users: userId
+    })
+
+    console.log(project)
+
+    if (!project) {
+        throw new Error("User not belong to this project")
+    }
+        const updatedProject = await Project.findOneAndUpdate({
+        _id: projectId
+    }, {
+        $addToSet: {
+            users: {
+                $each: users
+            }
+        }
+    }, {
+        new: true
+    })
+
+    return updatedProject
+  }
+  catch(error){
+console.log("error adding user:",error)
   }
 }
