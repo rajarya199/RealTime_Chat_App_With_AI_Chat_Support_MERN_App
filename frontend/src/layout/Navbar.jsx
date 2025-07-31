63b4dfa6606a60a9fe077d56717dfc7d50460449
rajarya199 <aryalrajan078@gmail.com>
2025-07-31 21:30:44 +0545

    set link

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -9,9 +9,12 @@ const Navbar = () => {
         <div className="flex justify-between h-16 items-center">
           <div className="flex items-center">
             <MessageSquareTextIcon className="h-8 w-8 text-indigo-400" />
-            <span className="ml-2 text-xl font-bold text-white">
+            <Link to="/">
+              <span className="ml-2 text-xl font-bold text-white">
               ChatSphere
             </span>
+            </Link>
+          
           </div>
           <nav className="hidden md:flex space-x-8">
             <a
@@ -34,7 +37,7 @@ const Navbar = () => {
             </a>
             <Link to='/myProject'
                           className="text-gray-300 hover:text-indigo-400 px-3 py-2 text-sm font-medium"
-            > Create Project
+            > My Project
             </Link>
           </nav>
           <div className="hidden md:flex items-center space-x-4">
