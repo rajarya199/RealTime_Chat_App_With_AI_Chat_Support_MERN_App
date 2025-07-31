fc533811e91b4ef86e24e1deacb8174c7d8d875b
rajarya199 <aryalrajan078@gmail.com>
2025-07-31 12:41:49 +0545

    socket event-

--- frontend/src/config/socket.js
+++ frontend/src/config/socket.js
@@ -1,10 +1,13 @@
 import socket from "socket.io-client"
 
 let socketInstance=null;
-export const initializeSocket=()=>{
+export const initializeSocket=(projectId)=>{
     socketInstance=socket(import.meta.env.VITE_API_URI,{
         auth:{
             token:localStorage.getItem('token')
+        },
+        query:{
+            projectId
         }
     })
      socketInstance.on("connect", () => {
@@ -15,4 +18,13 @@ export const initializeSocket=()=>{
     console.error("Socket connection error:", error.message);
   });
     return socketInstance
+}
+
+
+export const receiveMessage = (eventName, cb) => {
+    socketInstance.on(eventName, cb);
+}
+
+export const sendMessage = (eventName, data) => {
+    socketInstance.emit(eventName, data);
 }
