7e8c06fa78550e62c66ef01fb4c97185e21aba16
rajarya199 <aryalrajan078@gmail.com>
2025-08-09 22:47:08 +0545

    proxy config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,6 +12,14 @@ export default defineConfig({
   headers:{
     "Cross-Origin-Embedder-Policy": "require-corp",
 "Cross-Origin-Opener-Policy": "same-origin"
-  }
-  }
+  },
+     proxy: {
+      '/cdn': {
+        target: 'https://unpkg.com',
+        changeOrigin: true,       
+         rewrite: (path) => path.replace(/^\/cdn/, '')
+      }
+    }
+  },
+
 })
