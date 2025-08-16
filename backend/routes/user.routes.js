e1fa689b5248be845a0905e53ce731bee8d28dd3
rajarya199 <aryalrajan078@gmail.com>
2025-08-16 16:31:04 +0545

    user validation

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,18 +1,19 @@
 import {Router} from "express"
 import { createUserController,loginController,ProfileController,logoutController, getAllUsersController } from "../controllers/user.controller.js"
-import { body } from "express-validator";
 import  * as authmiddleware from"../middleware/auth.middleware.js"
+import { loginUserValidation,registerUserValidation } from "../validation/users.validator.js";
+import {validate} from"../validation/validator.middleware.js"
+
 const router=Router();
 router.post("/register",
-     body('email').isEmail().withMessage('Email must be a valid email address'),
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-body('username').isLength({min:3}).withMessage("username must be of at least 3 character")
-    ,createUserController)
+    registerUserValidation,
+    validate,
+    createUserController)
 
 
     router.post('/login',
-            body('email').isEmail().withMessage('Email must be a valid email address'),
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+loginUserValidation,
+validate,
     loginController
     )
 
