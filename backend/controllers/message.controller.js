14e18ea24082ae070085d234b1f1a9d91b95e4c3
rajarya199 <aryalrajan078@gmail.com>
2025-08-20 23:28:45 +0545

    save msg controller

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,54 @@
+import Project from "../models/project.model.js";
+import Message from "../models/message.model.js"
+export const saveMessage=async(req,res)=>{
+    try{
+        const{projectId,text,aiResponse,isAI}=req.body;
+        //validate fields
+           if (!projectId) {
+      return res.status(400).json({ error: "Project ID is required" });
+    }
+    if (!text || text.trim() === "") {
+      return res.status(400).json({ error: "Message text is required" });
+    }
+
+    //2 validate if project exist
+    const project=await Project.findById(projectId);
+     if (!project) {
+      return res.status(404).json({ error: "Project not found" });
+    }
+
+    //3 set sender info-user/ai
+    let sender;
+    if(isAI){
+          sender = {
+        id: "ai",
+        username: "AI",
+        email: "AI",
+      };
+    }else{
+         if (!req.user) {
+        return res.status(401).json({ error: "Unauthorized user" });
+      }
+      sender={
+         id: req.user.id,
+        username: req.user.username,
+        email: req.user.email,
+      } 
+    }
+
+    //4 save new message
+    const newMessage=await Message.create({
+        project:projectId,
+        sender,
+        text,
+        aiResponse:aiResponse || null,
+    });
+
+    res.status(201).json(newMessage);
+    }
+    catch(error){
+  console.error("Error creating message:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+    }
+
+}
