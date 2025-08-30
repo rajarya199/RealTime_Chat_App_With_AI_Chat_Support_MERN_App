31796edc9b4fb47416db9418e3bc26b00ddd8d04
rajarya199 <aryalrajan078@gmail.com>
2025-08-30 12:29:03 +0545

    route of msg

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,7 +1,14 @@
 import { Router } from 'express';
 import  * as authmiddleware from"../middleware/auth.middleware.js"
-import { saveMessage,getMessagesByProject } from '../controllers/message.controller.js';
+import { saveMessage,getMessagesByProject ,updateMessageFileTreeController} from '../controllers/message.controller.js';
+import {validate} from"../validation/validator.middleware.js"
+import { updateFileTreeValidation } from '../validation/message.validators.js';
 const router=Router()
 router.post("/save", authmiddleware.authUser, saveMessage);
 router.get("/project/:projectId",authmiddleware.authUser,getMessagesByProject)
+router.put("/update-file-tree",
+    authmiddleware.authUser,
+    updateFileTreeValidation,
+    validate,
+    updateMessageFileTreeController)
 export default router;
