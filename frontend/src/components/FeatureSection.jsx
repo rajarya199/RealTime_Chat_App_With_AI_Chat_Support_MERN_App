e6aa7425436cafc59c14a02c04e86835b468e58d
rajarya199 <aryalrajan078@gmail.com>
2025-09-04 23:15:49 +0545

    features section ui update

--- frontend/src/components/FeatureSection.jsx
+++ frontend/src/components/FeatureSection.jsx
@@ -33,7 +33,7 @@ const FeaturesSection = () => {
     },
   ]
   return (
-    <section id="features" className="py-16 bg-gray-800">
+    <section id="features" className="py-16 bg-gray-900">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center mb-16">
           <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
